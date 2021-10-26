5fca3b13a226de7993e95bc5ba9a408573b566d1
Sambit Kumar Nanda <sambitn20@gmail.com>
2021-10-26 17:51:38 +0530

    add a contoller and router for home

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 var express = require("express");
 var router = express.Router();
+const homeController = require("../controllers/home_controller");
 
 console.log("Router Loaded");
+
+router.get("/", homeController.home);
+
 module.exports = router;
