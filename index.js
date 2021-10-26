a2a9856c5d2d57ec5da069d8cebd3ca4fec8ac0e
Sambit Kumar Nanda <sambitn20@gmail.com>
2021-10-26 17:45:02 +0530

    first index router added

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+// USe express Router
+app.use("/", require("./routes"));
+
 app.listen(PORT, (err) => {
   if (err) {
     console.log(`Error: ${err}`);
