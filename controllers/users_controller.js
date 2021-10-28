9e06e1b8319b9b82a2a1e3704c892940e9f8fd0d
Sambit Kumar Nanda <sambitn20@gmail.com>
2021-10-28 15:58:23 +0530

    adding User Sign Up manual auth

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,5 @@
+const User = require("../models/user");
+
 module.exports.profile = (req, res) => {
   return res.render("user_profile", {
     title: "Profile",
@@ -17,9 +19,30 @@ module.exports.signIn = (req, res) => {
 };
 
 //Get the sign up data
+module.exports.create = function (req, res) {
+  if (req.body.password != req.body.confirm_password) {
+    return res.redirect("back");
+  }
 
-module.exports.create = (req, res) => {
-  //ToDO Later
+  User.findOne({ email: req.body.email }, function (err, user) {
+    if (err) {
+      console.log("error in finding user in signing up");
+      return;
+    }
+
+    if (!user) {
+      User.create(req.body, function (err, user) {
+        if (err) {
+          console.log("error in creating user while signing up");
+          return;
+        }
+
+        return res.redirect("/users/sign-in");
+      });
+    } else {
+      return res.redirect("back");
+    }
+  });
 };
 
 //signIN and create a session for the User
