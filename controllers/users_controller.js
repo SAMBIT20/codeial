66dcfdd12bd64a9043fb9cfb509ccbb85bcae64c
Sambit Kumar Nanda <sambitn20@gmail.com>
2021-11-01 10:36:20 +0530

    Adding user profile links

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,8 +1,11 @@
 const User = require("../models/user");
 
 module.exports.profile = (req, res) => {
-  return res.render("user_profile", {
-    title: "Profile",
+  User.findById(req.params.id, (err, user) => {
+    return res.render("user_profile", {
+      title: "Profile",
+      profile_user: user,
+    });
   });
 };
 
